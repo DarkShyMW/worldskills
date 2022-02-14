2e3ba05cd5da78a1f0eff6c6ee3d79ffa7b6064e
DarkShy <30442729+DarkShyMW@users.noreply.github.com>
2022-02-14 17:53:17 +0300

    Add files via upload

---
+++ ou+group.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory;
+
+$name = "IT", "Manager", "Supporter", "Overal", "Cloud storage", "Competitors";
+
+ForEach ($item in $name) {
+    New-ADOrganizationalUnit -Name $item;
+    New-ADGroup -Name $item -GroupScope Global -Path ("OU=" + $item + ",DC=skill39,DC=wsr");
+}
---
+++ sript.ps1
@@ -0,0 +1,19 @@
+﻿#Sorry, I'm newbie script kiddy
+#Fix my errors, please
+Import-Module ActiveDirectory
+Import-Csv C:\users.csv -Delimiter ";" | ForEach-Object {New-ADUser -Name ($_."Last Name" + $_."First Name")  `
+                                         -GivenName $_."First Name"                 `
+                                         -Surname $_."Last Name"                   `
+                                         -Title $_.Role                   `
+                                         -MobilePhone $_.Phone            `
+                                         -Department $_.OU                `
+                                         -StreetAddress $_.Street         `
+                                         -PostalCode $_.ZIP               `
+                                         -City $_.City                    `
+                                         -Country RU                      `
+                                         -Path ("OU=" + $_.OU + ",DC=skill39,DC=wsr") `
+                                         -AccountPassword(ConvertTo-SecureString("P@ssw0rd1") -AsPlainText -Force) `
+                                         -Enabled $true                    `
+                                         -CannotChangePassword $true}
+Import-Csv C:\users.csv -Delimiter ";" | ForEach-Object{Add-ADGroupMember -Identity $_.OU `
+                                         -Members ($_."Last Name" + $_."First Name") }
